0a8827fd6940c28ea85c737c6e45f22da144b884
松浦一樹 <matsuurakazuki@matsuurakazukinoiMac.local>
2020-06-12 00:21:33 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '58d8dce0ed423a35db4c247dd2ebdfea191fff06c02d35595ad2b965b41a997931335bfab0a7ed7170bc11baa2dadf5066f57a982b4a86f2a74f9a670ad9a21c'
