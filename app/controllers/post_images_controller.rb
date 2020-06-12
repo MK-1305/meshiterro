7007dd8f5ff768eead5c8980f918b8d3869c7da9
松浦一樹 <matsuurakazuki@matsuurakazukinoiMac.local>
2020-06-12 19:40:41 +0900

    [finish]5-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,15 +7,20 @@ class PostImagesController < ApplicationController
 		@post_image = PostImage.new(post_image_params)
 		@post_image.user_id = current_user.id
 		@post_image.save
-		redirect_to post_image_path(post_image)
+		if @post_image.save
+		   redirect_to post_image_path
+		else
+		   render :new
+		end
 	end
 
 	def index
-		@post_images = PostImage.all
+		@post_images = PostImage.page(params[:page]).reverse_order
 	end
 
 	def show
 		@post_image = PostImage.find(params[:id])
+		@post_comment = PostComment.new
 	end
 
 	private
